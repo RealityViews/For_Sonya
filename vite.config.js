9269ed53ff550b4fa04755ca0561e7b9a9957c9c
RealityViews <kuchma.sasha2001@gmail.com>
2025-05-14 17:08:34 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,7 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+export default defineConfig({
+  plugins: [react()],
+  base: '/For_Sonya/', // ← замени на имя твоего репо, если оно другое
+});
